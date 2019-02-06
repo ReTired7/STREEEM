82ad2cece18cde0f989d5b999d185d17d1f33732
ReTired7 <47307366+ReTired7@users.noreply.github.com>
2019-02-06 19:30:14 +0200

    Update BOT.js

--- BOT.js
+++ BOT.js
@@ -1,33 +1,14 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-//الاوامر//
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
 
-
-client.on("message", message => {
- if (message.content === "f!help") {
-  const embed = new Discord.RichEmbed() 
-      .setColor("#ffff00")
-      .setThumbnail(message.author.avatarURL)
-      .setDescription(`
-***
-ملاحظه
-Special orders ان برفكس الاوامر المميزه او
-
-يختلف عن جميع الاوامر 
-Special orders برفكس الاوامر المميزه او 
-f!!
-اما برفكس باقي الاوامر
-f!
-***
-`)
-
-
-message.author.sendEmbed(embed)
-
-}
-}); 
+client.login(process.env.BOT_TOKEN);
