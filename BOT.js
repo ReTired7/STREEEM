8973d5d69ed092961d00748c91fefb8dcfcab616
ReTired7 <47307366+ReTired7@users.noreply.github.com>
2019-02-06 18:16:29 +0200

    Update BOT.js

--- BOT.js
+++ BOT.js
@@ -2,14 +2,15 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 
+
 client.on('ready', () => {
    console.log(`----------------`);
-      console.log(`Desert Bot- Script By : Darks`);
+      console.log(`Desert Bot- Script By : Retir'ed`);
         console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By :Darks  ' `);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : Retir'ed ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`حط الكلام اللي تبغاه هنا`,"http://twitch.tv/S-F")
+client.user.setGame(`=help|BY Arab Unity`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
