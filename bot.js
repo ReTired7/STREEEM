e09497044040396cdbc1fb42b69c02666ba59b2f
ReTired7 <47307366+ReTired7@users.noreply.github.com>
2019-02-14 17:09:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,42 +5,6 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-if (message.content.startsWith(prefix + 'setwatch')) {
-  client.user.setActivity(argresult, {type: 'WATCHING'})
-     console.log('test' + argresult);
-    message.channel.sendMessage(Watch Now: **${argresult})
-} 
-
- 
-  if (message.content.startsWith(prefix + 'setlis')) {
-  client.user.setActivity(argresult, {type: 'LISTENING'})
-     console.log('test' + argresult);
-    message.channel.sendMessage(LISTENING Now: **${argresult})
-} 
-
-
-if (message.content.startsWith(prefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.sendMessage(Username Changed To **${argresult}**)
-  return message.reply("You Can change the username 2 times per hour");
-} 
-
-if (message.content.startsWith(prefix + 'setavatar')) {
-  client.user.setAvatar(argresult);
-   message.channel.sendMessage(Avatar Changed Successfully To **${argresult}**);
-}
-
-if (message.content.startsWith(prefix + 'setT')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/peery13%22);
-     console.log('test' + argresult);
-    message.channel.sendMessage(Streaming: **${argresult})
-} 
-if (message.content.startsWith(prefix + 'setgame')) {
-  client.user.setGame(argresult);
-     console.log('test' + argresult);
-    message.channel.sendMessage(Playing: **${argresult})
-}
-
 //الاوامر//
 
 
